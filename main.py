186fb6cf44eba17609a4b36e6d0c5a0185c38779
sarah castorena <ceanz@yahoo.com>
2022-10-17 22:26:57 -0700

    new

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ delval = int(input('Enter the deletion value: '))
 # Make your Code
 # ******************************
 count = numbers.count(delval)
+print(count)
 for i in range(count):
     numbers.remove(delval)
 
+else:
+     exit
    
 print (numbers)
